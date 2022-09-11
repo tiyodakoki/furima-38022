9ec73d7dacfad73b6cf65f29a2e648520f75176c
tiyodakoki <kagunatsu@gmail.com>
2022-09-11 14:19:46 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
+
 
 
   private
@@ -8,5 +12,8 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birth, :first_name_kana, :last_name_kana])
   end
 end
