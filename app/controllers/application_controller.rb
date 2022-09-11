9fc473f684a6ff1500517965530e436626ad1468
tiyodakoki <kagunatsu@gmail.com>
2022-09-11 18:52:34 +0900

    ログアウト機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
 
   
 
@@ -8,12 +11,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-    end
-  end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birth, :first_name_kana, :last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birth, :first_name_kana, :last_name_kana ])
   end
 end
