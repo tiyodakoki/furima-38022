c49d62532715a1de387fb3984a86d34d0570fb86
tiyodakoki <kagunatsu@gmail.com>
2022-09-13 16:40:32 +0900

    プルダウン実装まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create
   end
 end
