c91d2443ec0558e4bc5f5d05a23d0cd3d2d21951
tiyodakoki <kagunatsu@gmail.com>
2022-09-15 01:45:30 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,88 +11,93 @@ RSpec.describe Item, type: :model do
       end
     end
     context '出品できないとき' do
-    it 'nameが空では登録出来ない' do
-      @item.name = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
-    end
-    it 'imageが空では登録出来ない' do
-      @item.image = nil
-      @item.valid?
-      binding.pry
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
-    it 'informationが空では登録出来ない' do
-      @item.information = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Information can't be blank")
-    end
-    it 'category_idが空では登録出来ない' do
-      @item.category_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
-    it 'category_idが1では登録出来ない' do
-      @item.category_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
-    end
-    it 'condition_idが空では登録出来ない' do
-      @item.condition_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Condition can't be blank")
-    end
-    it 'condition_idが1では登録出来ない' do
-      @item.condition_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Condition can't be blank")
-    end
-    it 'postage_idが空では登録出来ない' do
-      @item.postage_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Postage can't be blank")
-    end
-    it 'postage_idが1では登録出来ない' do
-      @item.postage_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Postage can't be blank")
-    end
-    it 'region_idが空では登録出来ない' do
-      @item.region_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Region can't be blank")
-    end
-    it 'region_idが1では登録出来ない' do
-      @item.region_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Region can't be blank")
-    end
-    it 'shipping_day_idが空では登録出来ない' do
-      @item.shipping_day_id = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
-    end
-    it 'shipping_day_idが1では登録出来ない' do
-      @item.shipping_day_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
-    end
-    it 'priceが空では登録出来ない' do
-      @item.price = ''
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
-    it 'priceが300以下だと登録出来ない' do
-      @item.price = 100
-      @item.valid?
+      it 'nameが空では登録出来ない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'imageが空では登録出来ない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'informationが空では登録出来ない' do
+        @item.information = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Information can't be blank")
+      end
+      it 'category_idが空では登録出来ない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idが1では登録出来ない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが空では登録出来ない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'condition_idが1では登録出来ない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'postage_idが空では登録出来ない' do
+        @item.postage_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+      it 'postage_idが1では登録出来ない' do
+        @item.postage_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+      it 'region_idが空では登録出来ない' do
+        @item.region_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+      it 'region_idが1では登録出来ない' do
+        @item.region_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+      it 'shipping_day_idが空では登録出来ない' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'shipping_day_idが1では登録出来ない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'priceが空では登録出来ない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角では登録出来ない' do
+        @item.price = '９９９９'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
 
-      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
-    end
-    it 'priceが99999999以上だと登録出来ない' do
-      @item.price = 10_000_000_000_000
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
-     end
+      it 'priceが300以下だと登録出来ない' do
+        @item.price = 100
+        @item.valid?
+
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが99999999以上だと登録出来ない' do
+        @item.price = 10_000_000_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
     end
   end
 end
