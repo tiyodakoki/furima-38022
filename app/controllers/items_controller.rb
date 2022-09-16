b94db8872237d83420d688576f9b4c9c8f7cc54e
tiyodakoki <kagunatsu@gmail.com>
2022-09-16 20:33:19 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, ]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
